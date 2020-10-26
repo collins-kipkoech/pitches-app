0f2a0445b28225d8ed8785d9e527aa4764fb1896
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-10-26 12:49:02 +0300

    added the requirements file

--- app/email.py
+++ app/email.py
@@ -1,7 +1,7 @@
 from flask_mail import Message
 from flask import render_template
 from . import mail
-subject_pref = 'PitchesHub'
+subject_pref = 'PitchesApp'
 sender_email = 'colookipkoech@gmail.com'
 
 
